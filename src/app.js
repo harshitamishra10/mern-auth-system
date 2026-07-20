2b00241d1d7e98518d37644321908cb54da02fd6
harshita <mishraharshitaims25@gmail.com>
2026-07-20 16:39:36 +0530

    Fix CORS configuration

--- src/app.js
+++ src/app.js
@@ -6,10 +6,21 @@ import { authLimiter } from "./middleware/rateLimiter.js";
 
 const app = express();
 
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://mern-auth-system-frontend.onrender.com",
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
